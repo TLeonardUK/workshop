f0120a5ca493dce5a608cd148850409c5b50dfa5
Tim Leonard <tim@programmingbytim.com>
2023-07-05 13:45:52 +0100

    Remove cmake, rely on installed version.

--- engine/tools/scripts/generate_vs2022.bat
+++ engine/tools/scripts/generate_vs2022.bat
@@ -3,7 +3,7 @@
 SET ScriptPath=%~dp0
 SET RootPath=%ScriptPath%..\..\..\
 SET BuildPath=%ScriptPath%..\..\..\intermediate\vs2022\
-SET CMakeExePath=%ScriptPath%..\cmake\bin\cmake.exe
+SET CMakeExePath="C:\Program Files\CMake\bin\cmake.exe"
 
 echo Generating %RootPath%
 echo %CMakeExePath% -S %RootPath% -B %BuildPath%
